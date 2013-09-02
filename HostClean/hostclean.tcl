314bff65cd8b581d4748ea0251d34e00010a78d4
James Seward <james@jamesoff.net>
2013-09-02 15:47:30 -0400

    hostclean working properly

--- HostClean/hostclean.tcl
+++ HostClean/hostclean.tcl
@@ -11,6 +11,7 @@ proc load_hostnames { handle idx filename } {
 
 	while {![eof $filehandle]} {
 		set line [string trim $line]
+		set line "somenick!$line"
 		lappend seen_hostnames $line
 		incr count
 		set line [gets $filehandle]
@@ -31,9 +32,11 @@ proc hostclean { handle idx user } {
 				continue
 			}
 			set regexp_hostname [string map { . \\. * .+ ? . } $host]
+			regsub {\.\+!([a-zA-Z0-9_])} $regexp_hostname {.+!~?\1} regexp_hostname
+			regsub {\.\+!\.\+([a-zA-Z0-9_])} $regexp_hostname {.+!.*\1} regexp_hostname
 			set seen 0
 			foreach seen_host $seen_hostnames {
-				if [regexp $regexp_hostname $seen_host] {
+				if [regexp -nocase $regexp_hostname $seen_host] {
 					set seen 1
 					break
 				}
